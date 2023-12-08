9d61e07fb6b405d61a106b39fc5d95bd7f58d608
Vune <nguyenvu2319@gmail.com>
2023-12-09 03:24:51 +0700

    feat: import css bootstrap global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "../components/Header/page";
 import Footer from "../components/Footer/page";
