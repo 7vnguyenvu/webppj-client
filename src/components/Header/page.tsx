ca49b76a82e0c72cd263f8cedb125a613cb5dc2f
Vune <nguyenvu2319@gmail.com>
2023-12-09 03:19:53 +0700

    feat: config layout init and install bootstrap

---
+++ src/components/Header/page.tsx
@@ -0,0 +1,7 @@
+export default function Comp() {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
+}
---
+++ src/components/Navigation/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Comp() {
+    return <Link href="/dashboard">Dashboard</Link>;
+}
