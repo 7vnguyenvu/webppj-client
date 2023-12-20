fd1db634a94162e0b3ca44312f1af20556ffb4b0
Vune <nguyenvu2319@gmail.com>
2023-12-21 01:01:28 +0700

    feat: create user page

---
+++ src/app/[@nickname]/layout.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/page";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Header isHomePage={false} namepage="Trang cá nhân"></Header>
+            {children}
+        </>
+    );
+}
